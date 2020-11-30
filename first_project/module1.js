b1501dee1f9a900c67f6e7bea726f3818f00bde2
daniel <daniel.santana@onimusic.com.br>
2020-11-30 15:29:02 -0300

    many beginner's files for training

---
+++ first_project/module1.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  console.log('Function myFunction was called!');
+}
+
+var myString = 'Hi! My name is stringy!';
+
+module.exports.myFunction = myFunction;
+module.exports.myString = myString;
---
+++ first_project/module2.js
@@ -0,0 +1,6 @@
+module.exports = {
+  myFunction: function() {
+    console.log('This is the second module exported function');
+  },
+  mySecondString: 'This is the second exported string'
+};
